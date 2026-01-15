4f81d208a246b9ab2268f61841840f0864b2b1e6
Roman <nevolnik11@gmail.com>
2026-01-15 18:19:27 +0300

    Add files via upload

---
+++ JOIN user actions and orders for avg count prodacts in order.sql
@@ -0,0 +1,13 @@
+SELECT user_id
+             , ROUND(AVG(ARRAY_LENGTH(product_ids, 1)), 2) AS avg_order_size
+FROM
+    (SELECT DISTINCT order_id, user_id
+     FROM user_actions
+     WHERE action = 'create_order' AND order_id NOT IN (SELECT order_id
+                                                                                              FROM user_actions
+                                                                                              WHERE action = 'cancel_order')) AS ua
+    LEFT JOIN
+    orders AS ord
+    USING(order_id)
+GROUP BY user_id
+ORDER BY user_id
