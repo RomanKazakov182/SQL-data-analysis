859998a06725e11d82214c4085d9e1999ecfd668
Roman <nevolnik11@gmail.com>
2026-01-15 18:20:39 +0300

    Update JOIN user actions and orders for avg count prodacts in order.sql

--- JOIN user actions and orders for avg count prodacts in order.sql
+++ JOIN user actions and orders for avg count prodacts in order.sql
@@ -1,11 +1,11 @@
 SELECT user_id
-             , ROUND(AVG(ARRAY_LENGTH(product_ids, 1)), 2) AS avg_order_size
+     , ROUND(AVG(ARRAY_LENGTH(product_ids, 1)), 2) AS avg_order_size
 FROM
-    (SELECT DISTINCT order_id, user_id
+    (SELECT order_id, user_id
      FROM user_actions
      WHERE action = 'create_order' AND order_id NOT IN (SELECT order_id
-                                                                                              FROM user_actions
-                                                                                              WHERE action = 'cancel_order')) AS ua
+                                                        FROM user_actions
+                                                        WHERE action = 'cancel_order')) AS ua
     LEFT JOIN
     orders AS ord
     USING(order_id)
