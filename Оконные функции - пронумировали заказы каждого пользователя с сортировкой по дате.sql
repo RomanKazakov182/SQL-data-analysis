964b93a4e360a8381587e6db27d6abd058bfa49f
Roman <nevolnik11@gmail.com>
2026-01-28 07:47:09 +0300

    Add files via upload

---
+++ Оконные функции - пронумировали заказы каждого пользователя с сортировкой по дате.sql
@@ -0,0 +1,9 @@
+SELECT user_id
+     , order_id
+     , time
+     , ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY time) AS order_number
+FROM user_actions
+WHERE action = 'create_order'
+    AND order_id NOT IN (SELECT order_id FROM user_actions WHERE action = 'cancel_order')
+ORDER BY user_id, order_id
+LIMIT 1000
