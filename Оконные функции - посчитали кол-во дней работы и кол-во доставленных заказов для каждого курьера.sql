ef0b8adc340e79ea4a810a88637b69b36818dadf
Roman <nevolnik11@gmail.com>
2026-02-03 16:59:45 +0300

    Update Оконные функции - посчитали кол-во дней работы и кол-во доставленных заказов для каждого курьера.sql

--- Оконные функции - посчитали кол-во дней работы и кол-во доставленных заказов для каждого курьера.sql
+++ Оконные функции - посчитали кол-во дней работы и кол-во доставленных заказов для каждого курьера.sql
@@ -3,11 +3,10 @@ SELECT courier_id
      , MAX(last_action) - MIN(date) AS days_employed
 
 FROM
-(
-SELECT DISTINCT courier_id
-     , DATE(time) AS date
-     , COUNT(order_id) FILTER (WHERE action = 'deliver_order') OVER(PARTITION BY courier_id) AS delivered_orders
-     , MAX(DATE(time)) OVER() AS last_action
+(SELECT DISTINCT courier_id
+      , DATE(time) AS date
+      , COUNT(order_id) FILTER (WHERE action = 'deliver_order') OVER(PARTITION BY courier_id) AS delivered_orders
+      , MAX(DATE(time)) OVER() AS last_action
 FROM courier_actions
 ORDER BY courier_id) AS t
 GROUP BY courier_id, delivered_orders
