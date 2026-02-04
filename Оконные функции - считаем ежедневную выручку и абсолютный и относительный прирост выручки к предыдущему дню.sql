0003efd5efc5d4595fbeb4edca3bb25e0dbffce8
Roman <nevolnik11@gmail.com>
2026-02-04 08:39:11 +0300

    Update Оконные функции - считаем ежедневную выручку и абсолютный и относительный прирост выручки к предыдущему дню.sql

--- Оконные функции - считаем ежедневную выручку и абсолютный и относительный прирост выручки к предыдущему дню.sql
+++ Оконные функции - считаем ежедневную выручку и абсолютный и относительный прирост выручки к предыдущему дню.sql
@@ -1,31 +1,24 @@
 WITH revenue_data AS (
-    SELECT DATE(creation_time) AS date
-         , ROUND(SUM(price), 1) AS daily_revenue
-    FROM (
-        SELECT creation_time
-             , UNNEST(product_ids) AS product_id
-        FROM orders
-        WHERE order_id NOT IN (
-            SELECT order_id 
-            FROM user_actions 
-            WHERE action = 'cancel_order'
-        )
-    ) AS ords
-    LEFT JOIN products USING (product_id)
-    GROUP BY DATE(creation_time)
-),
+                     SELECT DATE(creation_time) AS date
+                          , ROUND(SUM(price), 1) AS daily_revenue
+                     FROM (
+                          SELECT creation_time
+                               , UNNEST(product_ids) AS product_id
+                          FROM orders
+                          WHERE order_id NOT IN (SELECT order_id FROM user_actions WHERE action = 'cancel_order')
+                          ) AS ords
+                     LEFT JOIN products USING (product_id)
+                     GROUP BY DATE(creation_time)
+                     ),
 revenue_with_lag AS (
-    SELECT date
-         , daily_revenue
-         , LAG(daily_revenue) OVER (ORDER BY date) AS prev_revenue
-    FROM revenue_data
-)
+                    SELECT date
+                         , daily_revenue
+                         , LAG(daily_revenue) OVER (ORDER BY date) AS prev_revenue
+                    FROM revenue_data
+                    )
 SELECT date
      , daily_revenue
      , COALESCE(ROUND(daily_revenue - prev_revenue, 1), 0) AS revenue_growth_abs
-     , COALESCE(
-         ROUND((daily_revenue - prev_revenue) / prev_revenue * 100, 1),
-         0
-       ) AS revenue_growth_percentage
+     , COALESCE(ROUND((daily_revenue - prev_revenue) / prev_revenue * 100, 1), 0) AS revenue_growth_percentage
 FROM revenue_with_lag
 ORDER BY date
