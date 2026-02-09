c070e603020df99cf2671e1dd7374af584225208
Roman <nevolnik11@gmail.com>
2026-02-09 16:09:42 +0300

    Update Оконные функции - на каждую дату считаем новых пользователей и курьеров + нарастающий итог.sql

--- Оконные функции - на каждую дату считаем новых пользователей и курьеров + нарастающий итог.sql
+++ Оконные функции - на каждую дату считаем новых пользователей и курьеров + нарастающий итог.sql
@@ -1,26 +1,26 @@
 WITH uni_cours AS
-(SELECT date
+(
+SELECT date
      , COUNT(courier_id) AS new_couriers
 FROM
-(
-SELECT courier_id
-     , DATE(MIN(time)) AS date
-FROM courier_actions
-GROUP BY courier_id
-) AS unique_couriers
+     (SELECT courier_id
+           , DATE(MIN(time)) AS date
+      FROM courier_actions
+      GROUP BY courier_id
+     ) AS unique_couriers
 GROUP BY date
 ORDER BY date)
 ,
 uni_usrs AS
-(SELECT date
+(
+SELECT date
      , COUNT(user_id) AS new_users
 FROM
-(
-SELECT user_id
-     , DATE(MIN(time)) AS date
-FROM user_actions
-GROUP BY user_id
-) AS unique_users
+     (SELECT user_id
+           , DATE(MIN(time)) AS date
+      FROM user_actions
+      GROUP BY user_id
+     ) AS unique_users
 GROUP BY date
 ORDER BY date)
 
